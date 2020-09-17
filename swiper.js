025d448572af758d52de3fae7e259d4a2a1e54bb
Stephen Fuchs <stephen@Stephens-MacBook-Pro.local>
2020-09-17 11:14:44 -0500

    pre-release

--- swiper.js
+++ swiper.js
@@ -9290,32 +9290,3 @@
 	return Swiper;
 
 })));
-
-var swiper = new Swiper('.swiper-container', {
-	autoHeight: true,
-	slidesPerView: 1,
-	spaceBetween: 30,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	// init: false,
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
-	breakpoints: {
-		768: {
-			slidesPerView: 2,
-			spaceBetween: 30,
-		},
-		992: {
-			slidesPerView: 3,
-			spaceBetween: 30,
-		},
-		1200: {
-			slidesPerView: 3,
-			spaceBetween: 30,
-		},
-	}
-});
