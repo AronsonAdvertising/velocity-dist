3c9b25c214a560d05ec06af9717673adc4b4d335
Stephen Fuchs <stephen@Stephens-MacBook-Pro.local>
2020-09-24 14:27:52 -0500

    0.4

--- trim-expand.js
+++ trim-expand.js
@@ -2,7 +2,7 @@ var waitForJQuery = setInterval(function () {
 	if (typeof $ != 'undefined') {
 
 		function change(id) {
-			var elem = document.getElementById('reveal' + id);
+			var elem = document.getElementById("reveal" + id);
 			if (elem.innerHTML == 'Collapse Key Features<span class="retract"></span>') elem.innerHTML = 'Expand Key Features<span class="expand"></span>';
 			else elem.innerHTML = 'Collapse Key Features<span class="retract"></span>';
 		}
